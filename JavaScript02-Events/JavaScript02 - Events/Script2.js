7a24e930516df981315183744859af546d179bbc
Garawanta Hurekity <Astronomyc9@gmail.com>
2024-08-22 21:32:10 +0900

    copied

--- JavaScript02-Events/JavaScript02 - Events/Script2.js
+++ JavaScript02-Events/JavaScript02 - Events/Script2.js
@@ -1,66 +1,72 @@
 ﻿// JavaScript source code
-const DARK = "#323232";
-const LIGHT = "#D8D8D8";
-let DELAY = 500; // Задержка появления картинки.
-// body.onload = function () { document.body.style.backgroundColor = LIGHT; }
-background_switch.onclick = function () {
-    //let switch_image = document.getElementById("background_switch");
-    //let filename = switch_image.currentSrc.split('/');
-    //console.log(filename);
-    //if (filename[filename.length - 1] === 'moon.png') {
-    //    switch_image.src = 'img/sun.png';
-    //    document.body.className = "dark";
-    ////    //document.body.style.backgroundColor = DARK;
-    ////    //document.body.style.color = LIGHT;
-    //}
-    //else {
-    //    switch_image.src = 'img/moon.png';
-    //    document.body.className = "light";
-    ////    //document.body.style.backgroundColor = LIGHT;
-    ////    //document.body.style.color = DARK;
-    //}
+const DARK = "#323232"
+const LIGHT = "#D8D8D8"
 
-     document.body.className = document.body.className === "light" ? "dark" : "light";
+//body.onload = function () { document.body.style.backgroundColor = LIGHT; }
+
+background_switch.onclick = function () {
+	//let switch_image = document.getElementById("background_switch");
+
+	//let filename = switch_image.currentSrc.split('/');
+	//console.log(filename);
+	//if (filename[filename.length - 1] === 'moon.png')
+	//{
+	//	switch_image.src = 'img/sun.png';
+	//	document.body.className = "dark";
+	//	//document.body.style.backgroundColor = DARK;
+	//	//document.body.style.color = LIGHT;
+	//}
+	//else
+	//{
+	//	switch_image.src = 'img/moon.png';
+	//	document.body.className = "light";
+	//	//document.body.style.backgroundColor = LIGHT;
+	//	//document.body.style.color = DARK;
+	//}
+
+	let backgroundDelay = document.getElementById("bkgDelay");
+	//let backgroundSwitch = document.getElementById("background_switch");
+	console.log(backgroundDelay.style);
+	let delay = backgroundDelay.value;
+	console.log(document.body.style);
+	document.body.style.transition = `background-color ${delay / 1000}s, color ${delay / 1000}s`;
+	document.getElementById("background_switch").style.transition = `background-image ${delay / 1000}s`;
+	console.log(document.body.style);
+	document.body.className = document.body.className === "light" ? "dark" : "light";
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////
-
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener("mousemove", function (event) {
-    //let x = event.clientX;
-    //let y = event.clientY;
-    //let c = document.querySelector("#coord");
-    //c.textContent = `X = ${x}, Y = ${y};`;
-    document.querySelector("#coord").textContent = `X = ${event.clientX}, Y = ${event.clientY};`;
+	//let x = event.clientX;
+	//let y = event.clientY;
+	//let c = document.querySelector("#coord");
+	//c.textContent = `X = ${x}, Y = ${y};`;
+	document.querySelector("#coord").textContent = `X = ${event.clientX}, Y = ${event.clientY};`;
 }
 );
 
-//////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
 function setImageVisibilityDelay() {
-
-    setTimeout("setImageVisibility()", DELAY);
+	let delay = document.getElementById("numDelay").value;
+	setTimeout("setImageVisibility()", Number(delay));
 }
-
-
-
-
 function setImageVisibility() {
-    let image = document.getElementById("image");
-    let button = document.getElementById("btnShowHide");
-    if (button.innerHTML === 'Скрыть') {
-        image.src = "img/transparent.png";
-        button.innerHTML = "Показать";
-    }
-    else {
-        image.src = "img/Drunkmonkey.jpg";
-        button.innerHTML = "Скрыть";
-    }
+	let image = document.getElementById("image");
+	let button = document.getElementById("btnShowHide");
+	if (button.innerHTML === 'Скрыть') {
+		image.src = "img/transparent.png";
+		button.innerHTML = "Показать";
+	}
+	else {
+		image.src = "img/DrunkMonkey.jpg";
+		button.innerHTML = "Скрыть";
+	}
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////
-
-document.write("<div id ='animated'> Something </div>");
+document.write("<div id='animated'>Ах у дуба...............</div>")
 let text = document.querySelector("#animated").innerHTML;
 let size = text.length;
 let i = 0;
@@ -68,17 +74,13 @@ let i = 0;
 window.addEventListener("load", animText);
 
 function animText() {
-    if (i >= size) return;
-    let id = document.querySelector("#animated");
-    id.innerHTML = text.substring(0, i++);
-    setTimeout(animText, 100);
+	if (i >= size) return;
+	let id = document.querySelector("#animated");
+	id.innerHTML = text.substring(0, i++);
+
+	setTimeout(animText, 100);
 }
 
-function delay_plus() {
-    DELAY += 100;
-}
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-function delay_minus() {
-    DELAY -= 100;
-    if (DELAY < 0) DELAY = 0; // чтобы задержка не уходила в минус, а то нелогично.
-}
+let date = new Date();
