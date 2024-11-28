6b067d5598a53e3b4d90ec71087a41b1aed2ea01
Garawanta Hurekity <Astronomyc9@gmail.com>
2024-11-28 22:33:56 +0900

    28.11 lesson done

--- React/intro/src/components/app/App.js
+++ React/intro/src/components/app/App.js
@@ -1,17 +1,31 @@
 
 import './App.css';
-import Article from '../article/Article';
 import Header from '../header/Header';
-import Footer from '../footer/Footer';
+import Nav from '../nav/Nav';
 import Power from '../power/power';
+import Article from '../article/Article';
+import Footer from '../footer/Footer';
+import data from '../article/db.json';
 
 function App() {
+
+  let nav = 
+  {
+    "Main" : "/index",
+    "News" : "/news",
+    "Store" : "/shop",
+    "About us" : "/about",
+    "Contacts" : "/contacts",
+  } //that's an array
+  
+let db = data;
+
   return (
     <div className="App">
       <Header title="Hello React" description="This is my first React App"/>
+      <Nav navigation = {nav}/>
       <Power a={2} n={8}/>
-      <Article/>
-      <Article/>
+      <Article db = {db}/>
       <Footer/>
     </div>
   );
--- React/intro/src/components/article/Article.js
+++ React/intro/src/components/article/Article.js
@@ -1,13 +1,17 @@
-function Article()
+function Article(props)
 {
+
+  console.log(props.db);
     return(
     <article>
-    <p>
+
+
+    {/* <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
       Eos animi ducimus natus consequatur vero, laboriosam nostrum
        praesentium inventore quo vitae excepturi itaque et omnis maxime 
        explicabo in beatae enim perspiciatis?      
-    </p>
+    </p> */}
   </article>)
 }
 
